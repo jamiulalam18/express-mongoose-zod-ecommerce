1f61c27c80bd5068c6d037a42d8cecbe5f434ec4
jamiulalam18 <jamiulalam18@gmail.com>
2024-06-05 19:56:58 -0400

    dependency install

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": "error",
+      "no-undef": "error",
+      "prefer-const": "error",
+      "no-console": "warn",
+    },
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node
+      },
+    },
+  },
+  {
+    ignores: ["**/node_modules/", "**/dist/"],
+  }
+);
